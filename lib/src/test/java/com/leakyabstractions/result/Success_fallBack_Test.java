86ad1586eb80a279569f85a230c22d9ec044b461
Guillermo Calvo <guillermo@guillermo.dev>
2022-09-10 14:03:01 +0200

    Add unit tests

---
+++ lib/src/test/java/com/leakyabstractions/result/Success_fallBack_Test.java
@@ -0,0 +1,32 @@
+
+package com.leakyabstractions.result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link Success#fallBack(Predicate, Function)}.
+ *
+ * @author Guillermo Calvo
+ */
+@DisplayName("Success fallBack")
+class Success_fallBack_Test {
+
+    @Test
+    void should_return_itself_no_matter_what() {
+        // Given
+        final Result<String, Integer> failure = new Success<>("SUCCESS");
+        final Predicate<Integer> isRecoverable = s -> fail("Should not happen");
+        final Function<Integer, String> mapper = s -> fail("Should not happen");
+        // When
+        final Result<String, Integer> result = failure.fallBack(isRecoverable, mapper);
+        // Then
+        assertThat(result).isSameAs(failure);
+    }
+}
